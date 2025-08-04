07fe897a55004db810cd999f44294df4e46a4a3f
ErycF <erycfreire.pro@gmail.com>
2025-08-04 00:00:27 -0300

    Solidity

--- Introdução ao solidity/Mappings, visibilidade e deploy.sol
+++ Introdução ao solidity/Mappings, visibilidade e deploy.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Heranca{
+
+    mapping (string => uint) Valor_a_Receber;
+
+    function Escreva_Valor(string memory _nome, uint _valor) public {
+        Valor_a_Receber[_nome] = _valor;
+    }
+
+    //Visibilidade: public, private, internal, external
+
+    function Pegar_Valor(string memory _nome) public view returns (uint){
     
+        return Valor_a_Receber[_nome];
+    }
 }
