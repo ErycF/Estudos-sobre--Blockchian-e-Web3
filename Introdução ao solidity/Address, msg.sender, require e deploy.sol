fd6615f3f743f9c23723f632870ca318a4a6c0e4
ErycF <erycfreire.pro@gmail.com>
2025-08-04 22:38:18 -0300

    Address, msg.sender, require e deploy

---
+++ Introdução ao solidity/Address, msg.sender, require e deploy.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Heranca_2{
+
+    mapping (string => uint) private Nome_Idade;
+
+    //address é um tipo de variável.
+    address public owner = msg.sender; // Aquele que envocou o contrato, quem chamou esse contrato. 
+
+    function Coloque_Seu_Nome_e_idade( string memory _nome, uint _idade ) public {
+        //require( msg.sender == owner); // ou require(owner == msg.sender);
+        require(owner == msg.sender);
+        Nome_Idade[_nome] = _idade; 
+    }
+
+    function Busca_idade( string memory Coloque_o_Nome) public view returns (uint){
+        return Nome_Idade[Coloque_o_Nome];
+    }
+}
