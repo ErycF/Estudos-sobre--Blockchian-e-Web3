c5046b014962bcc76d00d0f108576091a2e8f3a2
ErycF <erycfreire.pro@gmail.com>
2025-08-05 00:35:25 -0300

    deploy

--- Introdução ao solidity/Address, msg.sender, require e deploy.sol
+++ Introdução ao solidity/Address, msg.sender, require e deploy.sol
@@ -6,6 +6,7 @@ contract Heranca_2{
     mapping (string => uint) private Nome_Idade;
 
     //address é um tipo de variável.
+    //0x165e3dac16275d3aeb862fb27a127ed0e612193b address-contract
     address public owner = msg.sender; // Aquele que envocou o contrato, quem chamou esse contrato. 
 
     function Coloque_Seu_Nome_e_idade( string memory _nome, uint _idade ) public {
